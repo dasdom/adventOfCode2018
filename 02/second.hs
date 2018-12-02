a7536e89230c4e166440459266e0084ea9cc22df
Dominik Hauser <dom@dasdom.de>
2018-12-02 23:43:09 +0100

    Fix error in second puzzle, day two

--- 02/second.hs
+++ 02/second.hs
@@ -10,8 +10,11 @@ numberOfDifferences x y = length $ foldl appendIfDifferent [] (zip x y)
 appendIfDiffIsOne :: String -> [String] -> String -> [String]
 appendIfDiffIsOne x acc y = if numberOfDifferences x y == 1 then acc ++ [y] else acc
 
+
 main = do
     contents <- readFile "input.txt"
     let input = lines contents
-    let resultArray = foldl (\acc x -> acc ++ [head x]) [] (filter (\x -> length x > 0) (map (\x -> (foldl (appendIfDiffIsOne x) [] input )) input))
-    print $ intersect (head resultArray) (last resultArray)
+    let resultArray = foldl (\acc x -> if length (foldl (appendIfDiffIsOne x) [] input) > 0 then acc ++ [x] else acc) [] input
+    --print resultArray
+    --print $ intersect (head resultArray) (last resultArray)
+    print $ foldl (\acc x -> if fst x == snd x then acc ++ [fst x] else acc) [] $ zip (head resultArray) (last resultArray)
